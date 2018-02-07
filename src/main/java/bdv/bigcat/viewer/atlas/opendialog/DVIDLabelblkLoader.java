be5ff8f9be6d99c7ce82387939fc92c00340f680
Vanessa Leite <vrcleite@gmail.com>
2018-02-07 17:46:41 +0100

    Create different classes for uint and label -blk

---
+++ src/main/java/bdv/bigcat/viewer/atlas/opendialog/DVIDLabelblkLoader.java
@@ -0,0 +1,156 @@
+package bdv.bigcat.viewer.atlas.opendialog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.janelia.saalfeldlab.n5.DataBlock;
+import org.janelia.saalfeldlab.n5.DataType;
+import org.janelia.saalfeldlab.n5.LongArrayDataBlock;
+
+import net.imglib2.type.NativeType;
+
+public class DVIDLabelblkLoader< T extends NativeType< T > > extends DVIDLoader< T >
+{
+	public DVIDLabelblkLoader( String dvidURL, String repoUUID, String dataset, int[] blockSize, double[] voxelSize, double[] minPoint, DataType dataType )
+	{
+		super( dvidURL, repoUUID, dataset, blockSize, voxelSize, minPoint, dataType );
+	}
+
+	@Override
+	protected String createURL()
+	{
+		// labelblk:
+		// <api URL>/node/<UUID>/<data
+		// name>/raw/<dims>/<size>/<offset>[/<format>][?queryopts]
+
+		// Retrieves either 2d images (PNG by default) or 3d binary data,
+		// depending on the dims parameter.
+		// The 3d binary data response has "Content-type" set to
+		// "application/octet-stream" and is an array of
+		// voxel values in ZYX order (X iterates most rapidly).
+		// size: Size in voxels along each dimension specified in <dims>.
+		// offset: Gives coordinate of first voxel using dimensionality of data.
+
+		final StringBuffer buf = new StringBuffer( dvidURL );
+
+//		buf.append( "/api/node/" );
+//		buf.append( repoUUID );
+//		buf.append( "/" );
+//		buf.append( dataset );
+//		buf.append( "/blocks/" );
+//		buf.append( gridPosition[ 0 ] );
+//		buf.append( "_" );
+//		buf.append( gridPosition[ 1 ] );
+//		buf.append( "_" );
+//		buf.append( gridPosition[ 2 ] );
+//		buf.append( "/1" );
+
+		buf.append( "/api/node/" );
+		buf.append( repoUUID );
+		buf.append( "/" );
+		buf.append( dataset );
+		buf.append( "/raw/0_1_2/" );
+		buf.append( blockSize[ 0 ] );
+		buf.append( "_" );
+		buf.append( blockSize[ 1 ] );
+		buf.append( "_" );
+		buf.append( blockSize[ 2 ] );
+		buf.append( "/" );
+		buf.append( offset[ 0 ] );
+		buf.append( "_" );
+		buf.append( offset[ 1 ] );
+		buf.append( "_" );
+		buf.append( offset[ 2 ] );
+
+		return buf.toString();
+	}
+
+	@Override
+	protected DataBlock< ? > readBlock( String urlString ) throws IOException
+	{
+		final long[] data = new long[ blockSize[ 0 ] * blockSize[ 1 ] * blockSize[ 2 ] ];
+		final byte[] bytes = new byte[ data.length * 8 ];
+		final URL url = new URL( urlString );
+		final InputStream in = url.openStream();
+		int off = 0, l = 0;
+		do
+		{
+			l = in.read( bytes, off, data.length - off );
+			off += l;
+		}
+		while ( l > 0 && off < data.length );
+		in.close();
+
+		for ( int i = 0, j = -1; i < data.length; ++i )
+		{
+			data[ i ] =
+					( 0xffl & bytes[ ++j ] ) |
+							( ( 0xffl & bytes[ ++j ] ) << 8 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 16 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 24 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 32 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 40 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 48 ) |
+							( ( 0xffl & bytes[ ++j ] ) << 56 );
+		}
+
+		DataBlock< ? > dataBlock = new LongArrayDataBlock( blockSize, gridPosition, data );
+		return dataBlock;
+	}
+
+	private String createTileURL( final long[] gridPosition )
+	{
+		// <api URL>/node/<UUID>/<data name>/blocks/<size>/<offset>
+		// Error: io exception at url.openstream();
+		// Zero bytes downloaded
+
+		// Retrieves blocks corresponding to the extents specified by the size
+		// and offset. The subvolume request must be block aligned. This is the
+		// most server-efficient way of retrieving labelblk data, where data
+		// read from the underlying storage engine is written directly to the
+		// HTTP connection.
+		// size: Size in voxels along each dimension specified in <dims>.
+		// offset: Gives coordinate of first voxel using dimensionality of data.
+
+		final StringBuffer buf = new StringBuffer( dvidURL );
+
+		buf.append( "/api/node/" );
+		buf.append( repoUUID );
+		buf.append( "/" );
+		buf.append( dataset );
+		buf.append( "/blocks/" );
+		buf.append( blockSize[ 0 ] );
+		buf.append( "_" );
+		buf.append( blockSize[ 1 ] );
+		buf.append( "_" );
+		buf.append( blockSize[ 2 ] );
+		buf.append( "/" );
+		buf.append( offset[ 0 ] );
+		buf.append( "_" );
+		buf.append( offset[ 1 ] );
+		buf.append( "_" );
+		buf.append( offset[ 2 ] );
+
+		// https://github.com/google/neuroglancer/blob/98ee36743205c0a0b9dc8785dab3a5126805129b/src/neuroglancer/datasource/dvid/backend.ts#L77-L88
+		// Error: mal formed url
+		// this URL is not listed on the help of the uint<>blk and neither on labelblk.
+
+//		final StringBuffer buf = new StringBuffer( dvidURL );
+//
+//		buf.append( "/api/node/" );
+//		buf.append( repoUUID );
+//		buf.append( "/" );
+//		buf.append( dataset );
+//		buf.append( "/tile/0_1_2/" ); // 0_1_2 = dims
+//		buf.append( "1/" ); // level
+//		buf.append( gridPosition[ 0 ] );
+//		buf.append( "_" );
+//		buf.append( gridPosition[ 1 ] );
+//		buf.append( "_" );
+//		buf.append( gridPosition[ 2 ] );
+
+		return buf.toString();
+	}
+
+}
--- src/main/java/bdv/bigcat/viewer/atlas/opendialog/DVIDLoader.java
+++ src/main/java/bdv/bigcat/viewer/atlas/opendialog/DVIDLoader.java
@@ -1,11 +1,8 @@
 package bdv.bigcat.viewer.atlas.opendialog;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.function.BiConsumer;
 
-import org.janelia.saalfeldlab.n5.ByteArrayDataBlock;
 import org.janelia.saalfeldlab.n5.DataBlock;
 import org.janelia.saalfeldlab.n5.DataType;
 
@@ -21,21 +18,27 @@ import net.imglib2.type.numeric.integer.GenericShortType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 
-public class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
+public abstract class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
 {
-	private final String dvidURL;
+	protected final String dvidURL;
 
-	private final String repoUUID;
+	protected final String repoUUID;
 
-	private final String dataset;
+	protected final String dataset;
 
-	private final int[] blockSize;
+	protected final int[] blockSize;
 
-	private final double[] minPoint;
+	protected final double[] minPoint;
 
-	private final boolean isRaw;
+	protected long[] gridPosition;
 
-	private final BiConsumer< Img< T >, DataBlock< ? > > copyFromBlock;
+	protected long[] offset;
+
+	protected long[] cellDimensions;
+
+	protected long[] cellMin;
+
+	protected final BiConsumer< Img< T >, DataBlock< ? > > copyFromBlock;
 
 	public DVIDLoader(
 			final String dvidURL,
@@ -44,8 +47,7 @@ public class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
 			final int[] blockSize,
 			final double[] voxelSize,
 			final double[] minPoint,
-			final DataType dataType,
-			final boolean isRaw )
+			final DataType dataType )
 	{
 		this.dvidURL = dvidURL;
 		this.repoUUID = repoUUID;
@@ -53,155 +55,18 @@ public class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
 		this.blockSize = blockSize;
 		this.minPoint = minPoint;
 		this.copyFromBlock = createCopy( dataType );
-		this.isRaw = isRaw;
 	}
 
-	private DataBlock< ? > readBlock(
-			final String urlString,
-			final long[] gridPosition,
-			final byte[] data ) throws IOException
-	{
-		final URL url = new URL( urlString );
-		final InputStream in = url.openStream();
-		int off = 0, l = 0;
-		do
-		{
-			l = in.read( data, off, data.length - off );
-			off += l;
-		}
-		while ( l > 0 && off < data.length );
-		in.close();
+	protected abstract String createURL();
 
-		DataBlock< ? > dataBlock = new ByteArrayDataBlock( blockSize, gridPosition, data );
-		return dataBlock;
-	}
-
-	private String createRawBlocksURL( final long[] gridPosition )
-	{
-		// <api URL>/node/<UUID>/<data name>/blocks/<block coord>/<spanX>
-		// Retrieves "spanX" blocks of uncompressed voxel data along X starting
-		// from given block coordinate.
-		final StringBuffer buf = new StringBuffer( dvidURL );
-
-		buf.append( "/api/node/" );
-		buf.append( repoUUID );
-		buf.append( "/" );
-		buf.append( dataset );
-		buf.append( "/blocks/" );
-		buf.append( gridPosition[ 0 ] );
-		buf.append( "_" );
-		buf.append( gridPosition[ 1 ] );
-		buf.append( "_" );
-		buf.append( gridPosition[ 2 ] );
-		buf.append( "/1" );
-
-		return buf.toString();
-	}
-
-	private String createTileURL( final long[] gridPosition )
-	{
-		// https://github.com/google/neuroglancer/blob/98ee36743205c0a0b9dc8785dab3a5126805129b/src/neuroglancer/datasource/dvid/backend.ts#L77-L88
-		// this URL is not listed on the help of the uint<>blk and labelblk.
-
-		final StringBuffer buf = new StringBuffer( dvidURL );
-
-		buf.append( "/api/node/" );
-		buf.append( repoUUID );
-		buf.append( "/" );
-		buf.append( dataset );
-		buf.append( "/tile/0_1_2/" ); // 0_1_2 = dims
-		buf.append( "1/" ); // level
-		buf.append( gridPosition[ 0 ] );
-		buf.append( "_" );
-		buf.append( gridPosition[ 1 ] );
-		buf.append( "_" );
-		buf.append( gridPosition[ 2 ] );
-
-		return buf.toString();
-	}
-
-	private String createRawURL( final long[] offset )
-	{
-		// <api URL>/node/<UUID>/<data name>/raw/<dims>/<size>/<offset>
-		// Retrieves either 2d images (PNG by default) or 3d binary data,
-		// depending on the dims parameter.
-		// The 3d binary data response has "Content-type" set to
-		// "application/octet-stream" and is an array of voxel values in ZYX
-		// order (X iterates most rapidly).
-		// If the underlying data is float32, then the little-endian four byte
-		// format is written as RGBA.
-
-		final StringBuffer buf = new StringBuffer( dvidURL );
-
-		buf.append( "/api/node/" );
-		buf.append( repoUUID );
-		buf.append( "/" );
-		buf.append( dataset );
-		buf.append( "/raw/0_1_2/" ); // 0_1_2 = dims
-		buf.append( blockSize[ 0 ] );
-		buf.append( "_" );
-		buf.append( blockSize[ 1 ] );
-		buf.append( "_" );
-		buf.append( blockSize[ 2 ] );
-		buf.append( "/" );
-		buf.append( offset[ 0 ] );
-		buf.append( "_" );
-		buf.append( offset[ 1 ] );
-		buf.append( "_" );
-		buf.append( offset[ 2 ] );
-
-		return buf.toString();
-	}
-
-	private String createURL( final long[] offset )
-	{
-		// labelblk:
-		// <api URL>/node/<UUID>/<data name>/blocks/<size>/<offset>
-
-		// uint:
-		// <api URL>/node/<UUID>/<data name>/subvolblocks/<size>/<offset>
-		// Expected internal block data to be JPEG, was LZ4 compression instead
-
-		// Retrieves blocks corresponding to the extents specified by the size
-		// and offset. The subvolume request must be block aligned. This is the
-		// most server-efficient way of retrieving labelblk data, where data
-		// read from the underlying storage engine is written directly to the
-		// HTTP connection.
-		// size: Size in voxels along each dimension specified in <dims>.
-		// offset: Gives coordinate of first voxel using dimensionality of data.
-
-		final StringBuffer buf = new StringBuffer( dvidURL );
-
-		buf.append( "/api/node/" );
-		buf.append( repoUUID );
-		buf.append( "/" );
-		buf.append( dataset );
-
-		if ( isRaw )
-			buf.append( "/subvolblocks/" );
-		else
-			buf.append( "/blocks/" );
-
-		buf.append( blockSize[ 0 ] );
-		buf.append( "_" );
-		buf.append( blockSize[ 1 ] );
-		buf.append( "_" );
-		buf.append( blockSize[ 2 ] );
-		buf.append( "/" );
-		buf.append( offset[ 0 ] );
-		buf.append( "_" );
-		buf.append( offset[ 1 ] );
-		buf.append( "_" );
-		buf.append( offset[ 2 ] );
-
-		return buf.toString();
-	}
+	protected abstract DataBlock< ? > readBlock(
+			final String urlString ) throws IOException;
 
 	@Override
 	public void load( final SingleCellArrayImg< T, ? > cell )
 	{
-		final long[] gridPosition = new long[ cell.numDimensions() ];
-		final long[] offset = new long[ cell.numDimensions() ];
+		gridPosition = new long[ cell.numDimensions() ];
+		offset = new long[ cell.numDimensions() ];
 		for ( int d = 0; d < gridPosition.length; ++d )
 		{
 			gridPosition[ d ] = cell.min( d ) / blockSize[ d ];
@@ -209,17 +74,10 @@ public class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
 		}
 
 		final DataBlock< ? > block;
-		final byte[] data = new byte[ blockSize[ 0 ] * blockSize[ 1 ] * blockSize[ 2 ] ];
-
 		try
 		{
-			final String urlString;
-			if (isRaw)
-				urlString = createRawBlocksURL( gridPosition );
-			else
-				urlString = createRawURL( offset );
-
-			block = readBlock( urlString, gridPosition, data );
+			final String urlString = createURL();
+			block = readBlock( urlString );
 		}
 		catch ( final IOException e )
 		{
@@ -265,7 +123,6 @@ public class DVIDLoader< T extends NativeType< T > > implements CellLoader< T >
 		case UINT64:
 			return ( a, b ) -> {
 				final long[] data = ( long[] ) b.getData();
-				System.out.println( "data: " + data );
 				@SuppressWarnings( "unchecked" )
 				final Cursor< ? extends GenericLongType< ? > > c = ( Cursor< ? extends GenericLongType< ? > > ) a.cursor();
 				for ( int i = 0; i < data.length; ++i )
