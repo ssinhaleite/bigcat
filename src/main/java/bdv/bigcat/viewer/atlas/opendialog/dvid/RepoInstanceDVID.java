93b4f041b2c8e475eeb2fbd60b5d21bd6371e7cc
Vanessa Leite <vrcleite@gmail.com>
2018-02-13 16:51:57 +0100

    Add class for repository information

---
+++ src/main/java/bdv/bigcat/viewer/atlas/opendialog/dvid/RepoInstanceDVID.java
@@ -0,0 +1,20 @@
+package bdv.bigcat.viewer.atlas.opendialog.dvid;
+
+import java.util.Map;
+
+public class RepoInstanceDVID
+{
+	public String Root;
+
+	public String Alias;
+
+	public String Description;
+
+	public String[] Log;
+
+	public Map< String, DatasetInstanceDVID > DataInstances;
+
+	public String Created;
+
+	public String Updated;
+}
